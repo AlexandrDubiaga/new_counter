e676260fd4e2ed050ae6a425ca04af417a88ad32
Alex <alexandr.dubiaga@gmail.com>
2019-12-02 15:09:17 +0200

    Updates

--- precache-manifest.6ace797d881ed438aed0267641819128.js
+++ precache-manifest.6ace797d881ed438aed0267641819128.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c9f1a13b4820d7f3cb4ead1cd380f360",
+    "revision": "db1b097312ea87478289addcb7b3a2c2",
     "url": "/new_counter/index.html"
   },
   {
-    "revision": "03841491f09f642e7d68",
-    "url": "/new_counter/static/css/main.b141bb85.chunk.css"
+    "revision": "87a5a109cbc35007d507",
+    "url": "/new_counter/static/css/main.d8dab98d.chunk.css"
   },
   {
     "revision": "a32c0dfd2195ce3a86c1",
     "url": "/new_counter/static/js/2.6db76fd7.chunk.js"
   },
   {
-    "revision": "03841491f09f642e7d68",
-    "url": "/new_counter/static/js/main.534ad821.chunk.js"
+    "revision": "87a5a109cbc35007d507",
+    "url": "/new_counter/static/js/main.ac2e54e4.chunk.js"
   },
   {
     "revision": "30d23c8c096b5059abb4",
