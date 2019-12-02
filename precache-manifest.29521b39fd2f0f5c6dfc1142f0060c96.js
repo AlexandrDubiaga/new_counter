138977434bf5b164404d49aa8226dda006d68d50
Alex <alexandr.dubiaga@gmail.com>
2019-12-02 11:51:30 +0200

    Updates

---
+++ precache-manifest.29521b39fd2f0f5c6dfc1142f0060c96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85aa108b5c036fe6cfa471dbc498e095",
+    "url": "/new_counter/index.html"
+  },
+  {
+    "revision": "518177a518b69c659c0d",
+    "url": "/new_counter/static/css/main.0bdf1b95.chunk.css"
+  },
+  {
+    "revision": "a32c0dfd2195ce3a86c1",
+    "url": "/new_counter/static/js/2.6db76fd7.chunk.js"
+  },
+  {
+    "revision": "518177a518b69c659c0d",
+    "url": "/new_counter/static/js/main.a6745e93.chunk.js"
+  },
+  {
+    "revision": "30d23c8c096b5059abb4",
+    "url": "/new_counter/static/js/runtime-main.46aaa2e0.js"
+  }
+]);
